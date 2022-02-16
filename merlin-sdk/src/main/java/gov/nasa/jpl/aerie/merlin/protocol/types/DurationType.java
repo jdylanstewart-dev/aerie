72e3828454ba362ba4958aeedd219c950f3b6202
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-02-16 06:20:25 -0800

    Add Controllable variant to DurationType

--- merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/types/DurationType.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/types/DurationType.java
@@ -1,5 +1,6 @@
 package gov.nasa.jpl.aerie.merlin.protocol.types;
 
 public sealed interface DurationType {
+  record Controllable(String parameterName) implements DurationType {}
   record Uncontrollable() implements DurationType {}
 }
